54ffea57023daa9e5f195c158e36828f65810b69
Alynson Roldan <alynsonrolfu@gmai.com>
2022-11-11 16:26:29 -0300

    nosequehice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import pagina from './component/pagina.js';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Alynson Natalia Roldan Fuentes</h1>
     </div>
   );
 }
